5ec5f0c4711d423fe613bb9e4976c994a9180aca
networkdavit <lonecreation1612@gmail.com>
2021-03-24 13:34:42 +0400

    adding a change

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
 # flaskapp.py
 # This is a "hello world" app sample for flask app. You may have a different file.
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+
 app = Flask(__name__)
+
 @app.route('/') 
 def hello_world():
-   return 'Hello from Flask!' 
+   return render_template("index.html") 
+
+@app.route('/<page_name>')
+def page(page_name):
+    return render_template(f"{page_name}")
+
+@app.route("/receive_data", methods=['POST'])
+def receive_data():
+	if request.method == "POST":
+		email = request.form["email"]
+		subject = request.form["subject"]
+		message = request.form["message"]
+		data = f"Email: {email} \nSubject: {subject} \nMessage: {message}\n    "
+		f = open("message.txt", "a")
+		f.write(data)
+		f.close()
+		return redirect("index.html")
+
+
 if __name__ == '__main__':
    app.run()
+   
