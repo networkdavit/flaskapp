09a0d588c0a6102e7572eb269cc0c6662c8da300
networkdavit <lonecreation1612@gmail.com>
2021-03-26 08:34:50 +0400

    adding database

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 # flaskapp.py
 # This is a "hello world" app sample for flask app. You may have a different file.
 from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///submission.db"
+db = SQLAlchemy(app)
+
+class Submission(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	email = db.Column(db.String(200), nullable = False)
+	subject = db.Column(db.String(200), nullable = False)
+	message = db.Column(db.String(200), nullable = False)
+
+	def __repr__(self):
+		return "<Submission %r>" % self.id
 
 @app.route('/') 
 def hello_world():
@@ -18,11 +30,21 @@ def receive_data():
 		email = request.form["email"]
 		subject = request.form["subject"]
 		message = request.form["message"]
-		data = f"Email: {email} \nSubject: {subject} \nMessage: {message}\n    "
-		f = open("message.txt", "a")
-		f.write(data)
-		f.close()
-		return redirect("index.html")
+#		data = f"Email: {email} \nSubject: {subject} \nMessage: {message}\n    "
+#		f = open("message.txt", "a")
+#		f.write(data)
+#		f.close()
+#		return redirect("index.html")
+		submission = Submission(email = email, subject = subject, message = message)
+
+		try:
+			db.session.add(submission)
+			db.session.commit()
+			return redirect("thankyou")
+		except:
+			return "did not save to database"
+	else:
+		return "something went wrong"
 
 
 if __name__ == '__main__':
